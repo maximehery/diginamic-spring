292cf3596be61c859b900e15ef561f5d259ebad2
Maxime HERY <maximehery@maxime.home>
2025-04-07 14:51:17 +0200

    first commit

---
+++ src/test/java/fr/diginamic/diginamicspring/DiginamicSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.diginamic.diginamicspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiginamicSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
