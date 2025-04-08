3e825677aa800e5f8ace3b4d823bda1a842d7872
maximehery <maximehery18@outlook.fr>
2025-04-08 10:14:34 +0200

    tp2

--- src/main/java/fr/diginamic/diginamicspring/controllers/HelloController.java
+++ src/main/java/fr/diginamic/diginamicspring/controllers/HelloController.java
@@ -1,5 +1,7 @@
 package fr.diginamic.diginamicspring.controllers;
 
+import fr.diginamic.diginamicspring.services.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/hello")
 public class HelloController {
+    @Autowired
+    private HelloService helloService;
+
     @GetMapping
     public String hello() {
-        return "Hello World!";
+        return helloService.salutations();
     }
 }
